5e30b107d57209cd4d567bde3b4886bf5eaee062
Henry Li <henry1943@163.com>
2017-02-15 16:37:36 +0800

    Change the hidden unit count to 2X

--- lstm-rnn/main.py
+++ lstm-rnn/main.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 from input_data import InputData
 
 LEARNING_RATE = 0.001
-TRAIN_ITERATION_COUNT = 250
+TRAIN_ITERATION_COUNT = 100
 TRAIN_BATCH_SIZE = 100                      # How many sequences are there in a single training batch
 MAX_SEQ_LEN = 20                            # Length of the longest words
 
 INPUT_RANK = len(InputData.ALPHABETS)       # Rank of the input, here we use a 10-dim one-hot data as input,
 OUTPUT_RANK = INPUT_RANK                    # and the same form as output
-HIDDEN_UNIT_COUNT = OUTPUT_RANK * 2         # How many hidden units are there inside a RNN cell
+HIDDEN_UNIT_COUNT = OUTPUT_RANK * 4         # How many hidden units are there inside a RNN cell
 
 
 x = tf.placeholder(tf.float32, [None, MAX_SEQ_LEN, INPUT_RANK], name='x_input')
@@ -26,7 +26,7 @@ biases = {
 }
 
 
-def rnn(inputs, seq_len_of_inputs, weights, biases, batch_size=1):
+def rnn(inputs, seq_len_of_inputs, weights, biases):
     # inputs[?, MAX_SEQ_LEN, INPUT_RANK] => [? * MAX_SEQ_LEN, INPUT_RANK]
     inputs = tf.reshape(inputs, [-1, INPUT_RANK])
     # cell_inputs[? * MAX_SEQ_LEN, CELL_SIZE]
@@ -34,13 +34,12 @@ def rnn(inputs, seq_len_of_inputs, weights, biases, batch_size=1):
     # cell_inputs[?, MAX_SEQ_LEN, CELL_SIZE]
     cell_inputs = tf.reshape(cell_inputs, [-1, MAX_SEQ_LEN, HIDDEN_UNIT_COUNT])
     cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNIT_COUNT, forget_bias=biases['forget'])
-    cell_initial_state = cell.zero_state(batch_size, tf.float32)
-    cell_outputs, cell_states = tf.nn.dynamic_rnn(cell, cell_inputs, sequence_length=seq_len_of_inputs, initial_state=cell_initial_state)
+    cell_outputs, cell_states = tf.nn.dynamic_rnn(cell, cell_inputs, sequence_length=seq_len_of_inputs, dtype=tf.float32)
     cell_c_states, cell_m_states = cell_states
     results = tf.matmul(cell_m_states, weights['out']) + biases['out']
     return results
 
-pred = rnn(x, seq_len_of_x, weights, biases, TRAIN_BATCH_SIZE)
+pred = rnn(x, seq_len_of_x, weights, biases)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
 train = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cost)
 
